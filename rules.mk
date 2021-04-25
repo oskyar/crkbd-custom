4324e5c7de8a13437262027bd50268d8b7c5a410
Óscar Zafra <oskyar@gmail.com>
2021-04-25 19:48:56 +0200

    version inestable for game layer

---
+++ rules.mk
@@ -0,0 +1,10 @@
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = yes
+MOUSEKEY_ENABLE    = no
+NKRO_ENABLE        = no
+OLED_DRIVER_ENABLE = yes
+EXTRAKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
+VIA_ENABLE = yes
+# using elite-c controllers
+BOOTLOADER = qmk-dfu
